52b6d785e2e033ec976a1d27209c5b4bb158b068
THE'Y STUDIO <studio@theystudiodesign.com>
2026-07-12 20:45:16 +0000

    Gestion: Factures PDF Swiss style + rappel backup automatique (7 jours) + SW v6

--- gestion/sw.js
+++ gestion/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'they-gestion-v5';
+const CACHE = 'they-gestion-v6';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
