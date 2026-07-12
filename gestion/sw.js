e5be78de4b2bf951bf1733c8ea8366d12d69468e
THE'Y STUDIO <studio@theystudiodesign.com>
2026-07-12 23:11:12 +0000

    Dashboard 2.0: KPIs (aujourd'hui, annee, retards, valeur moy/projet, projets termines) + graphique revenus 12 mois en SVG natif multi-devises, compatible dark mode. SW v10

--- gestion/sw.js
+++ gestion/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'they-gestion-v9';
+const CACHE = 'they-gestion-v10';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
