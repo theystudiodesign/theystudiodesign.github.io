a140dd724ac1d0668dfaa5477760263bd1b56c2a
THE'Y STUDIO <studio@theystudiodesign.com>
2026-07-12 20:54:06 +0000

    Facture officielle Auto-Entrepreneur (taxe 1%, TTC, montant en lettres, RIB/ICE) + Bon de livraison + champ ICE client + SW v7

--- gestion/sw.js
+++ gestion/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'they-gestion-v6';
+const CACHE = 'they-gestion-v7';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
